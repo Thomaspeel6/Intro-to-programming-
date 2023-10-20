1485d0a065b865acd7e26e1634ab0e6950f639d3
Thomaspeel6 <146444012+Thomaspeel6@users.noreply.github.com>
2023-10-20 16:14:42 +0000

    fkjgfd

---
+++ test.py
@@ -0,0 +1,26 @@
+def valid_puzzle(puzzle: list) -> bool:
+   for item in enumerate(puzzle):
+    length_item = len(item)
+    if length_item 
+    pass
+
+
+
+    # sample test for task 1.1
+    puzzle1 = ['RUNAROUNDDL', 'EDCITOAHCYV', 'ZYUWSWEDZYA', 'AKOTCONVOYV',
+               'LSBOSEVRUCI', 'BOBLLCGLPBD', 'LKTEENAGEDL', 'ISTREWZLCGY',
+               'AURAPLEBAYG', 'RDATYTBIWRA', 'TEYEMROFINU']
+
+    puzzle2 = ['NAROUNDDL', 'EDCIT', 'UWSWEDZYA', 'OTCONVOYV',
+               'BOSEVRUCI', 'BLLCGLPBD', 'TEENAGEDL', 'TREWZLCGY',
+               'RAPLEBAYG', 'ATYTBIWRA', 'YEMROFINU']
+
+    puzzle3 = ['RUNAROU', ['EDCITOA'], ('ZYUWSWE'), 'AKOTCYV',
+               'LSBOSEI', 'BOBLLCG', 'LKTEENA', 'ISTREWY',
+               'AURAPLE', 'RDATYTB', 'TEYEMRO']
+    puzzle4 = 'roundandround'
+
+    print(valid_puzzle(puzzle1))
+    print(valid_puzzle(puzzle2))
+    print(valid_puzzle(puzzle3))
+    print(valid_puzzle(puzzle4))
